c638cb2c88714462e81cd9aa9858f0e298fbea8f
MemoryM <72047128+mmemory84@users.noreply.github.com>
2021-09-28 14:26:29 -0500

    Component 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -6,9 +6,11 @@ function Directory(props) {
 
     const renderDirectoryItem = ({item}) => {
         return (
+            
             <ListItem
                 title={item.name}
                 subtitle={item.description}
+                onPress={() => props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/react-lake.jpg')}}
             />
         );
