cdba32499bb5aa1784942e26588377a6908121ad
MemoryM <72047128+mmemory84@users.noreply.github.com>
2021-10-09 15:29:02 -0500

    workshop 2

--- redux/comments.js
+++ redux/comments.js
@@ -6,7 +6,21 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
             return {...state, errMess: null, comments: action.payload};
 
         case ActionTypes.COMMENTS_FAILED:
-            return {...state, errMess: action.payload};
+        return {...state, errMess: action.payload};
+
+
+        case ActionTypes.ADD_COMMENT:
+            const comment = {...action.payload, id: state.comments.length}
+            const comments = [...state.comments, comment]
+            return {...state, errMess: null, comments};
+
+
+
+        
+
+        
+
+        
 
         default:
             return state;
